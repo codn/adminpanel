af6ce214575306717fbef97c37f86374621bcbef
Jose Ramon Camacho <joserracamacho@gmail.com>
2014-01-10 18:33:43 -0600

    remove adminpanel.rb unnecesary require, remove non-default routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,18 +18,17 @@ Adminpanel::Engine.routes.draw do
             resources controllers.sub!('.rb', '').to_sym
         end
     end
-	resources :sections, :except => [:new]
-    resources :users
-    resources :galleries do
-    	member do
-    		put :move_better, :as => "move_to_better"
-    		put :move_worst, :as => "move_to_worst"
-    	end
-    end
-    resources :sessions, :only => [:new, :create, :destroy]
- #    resources :products
-    resources :categories
-    root :to => 'products#index'
+	resources :sections, :except => [:new, :create, :destroy]
     match '/signout', :to => 'sessions#destroy', :via => :delete, :as => "signout"
     match '/signin', :to => 'sessions#new', :as => "signin"
+    resources :users
+    resources :galleries do
+        member do
+            put :move_better, :as => "move_to_better"
+            put :move_worst, :as => "move_to_worst"
+        end
+    end
+    resources :sessions, :only => [:new, :create, :destroy]
+
+    root :to => 'galleries#index'
 end
--- lib/adminpanel.rb
+++ lib/adminpanel.rb
@@ -1,6 +1,5 @@
 require "adminpanel/version"
 require "adminpanel/engine"
-require "adminpanel/router"
 
 module Adminpanel
  class AccessDenied < StandardError
