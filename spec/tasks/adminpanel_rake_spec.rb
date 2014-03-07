3ce8ab82687baafc86a3a17a6c5abbe0c2cea87e
joseramonc <joserracamacho@gmail.com>
2014-03-07 11:41:57 -0600

    adds adminpanel:populate rake task and tests

---
+++ spec/tasks/adminpanel_rake_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "adminpanel namespace task" do
+
+  before :all do
+    Rake.application.rake_require "tasks/adminpanel/adminpanel"
+    Rake::Task.define_task(:environment)
+  end
+
+  describe "adminpanel:populate[20, product, name:name description:lorem price:number]" do
+
+    let(:has_nil_attribute) { false }
+    before do
+      Adminpanel::Product.delete_all
+      Rake.application.invoke_task "adminpanel:populate[20, product, name:name description:lorem price:number]"
+    end
+
+
+    it "should generate 20 product records" do
+      Adminpanel::Product.count.should eq 20
+    end
+
+    it "attributes shouldn't be nil" do
+      Adminpanel::Product.all.each do |product|
+        if (product.name.nil? || product.description.nil? || product.price.nil? ||
+          product.name == "" || product.description == "" || product.price == "")
+          has_nil_attribute = true
+        end
+      end
+      has_nil_attribute.should eq false
+    end
+  end
+end
