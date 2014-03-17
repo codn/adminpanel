662fdea88e253b8b95571d5305d171132dbd91cf
joseramonc <joserracamacho@gmail.com>
2014-03-17 15:08:38 -0600

    changes for 2 spaces

--- app/helpers/adminpanel/router_helper.rb
+++ app/helpers/adminpanel/router_helper.rb
@@ -1,33 +1,33 @@
 module Adminpanel
-    module RouterHelper
-        def adminpanel_resources
-            @files ||= find_resources
-        end
-
-        def find_resources
-            resources_path
-            if File.directory?(resources_path)
-                files = Dir.entries(resources_path).collect do |f|
-                    unless default_controllers.include?(f)
-                        file_path = "#{resources_path}#{f}"
-                        File.file?(file_path) ? file_path.sub!(resources_path, '').sub!('_controller.rb', '') : nil
-                    end
-                end
-
-                files.compact!
-            end
-        end
-
-        def menu_items
-            @menu ||= adminpanel_resources.each.collect { |resource| resource.classify }
-        end
-
-        def default_controllers
-            ["application_controller.rb", "sessions_controller.rb", "galleries_controller.rb", "users_controller.rb", "sections_controller.rb", "pages_controller.rb"]
-        end
-
-        def resources_path
-            "#{Rails.root.to_s}/app/controllers/adminpanel/"
-        end
+  module RouterHelper
+    def adminpanel_resources
+      @files ||= find_resources
     end
+
+    def find_resources
+      resources_path
+      if File.directory?(resources_path)
+        files = Dir.entries(resources_path).collect do |f|
+          unless default_controllers.include?(f)
+            file_path = "#{resources_path}#{f}"
+            File.file?(file_path) ? file_path.sub!(resources_path, '').sub!('_controller.rb', '') : nil
+          end
+        end
+
+        files.compact!
+      end
+    end
+
+    def menu_items
+      @menu ||= adminpanel_resources.each.collect { |resource| resource.classify }
+    end
+
+    def default_controllers
+      ["application_controller.rb", "sessions_controller.rb", "galleries_controller.rb", "users_controller.rb", "sections_controller.rb", "pages_controller.rb"]
+    end
+
+    def resources_path
+      "#{Rails.root.to_s}/app/controllers/adminpanel/"
+    end
+  end
 end
