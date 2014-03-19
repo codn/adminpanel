c1235c66b1667dc96ffa24d9aa6b0ea3145e4758
joseramonc <joserracamacho@gmail.com>
2014-03-19 10:40:07 -0600

    updates migration tests and generator

--- spec/tasks/adminpanel_rake_spec.rb
+++ spec/tasks/adminpanel_rake_spec.rb
@@ -7,16 +7,16 @@ describe "adminpanel rake task" do
     Rake::Task.define_task(:environment)
   end
 
-  describe "adminpanel:populate[20, product, name:name description:lorem price:number]" do
+  describe "adminpanel:populate[10, product, name:name description:lorem price:number]" do
 
     let(:has_nil_attribute) { false }
     before do
-      Rake.application.invoke_task "adminpanel:populate[20, product, name:name description:lorem price:number]"
+      Rake.application.invoke_task "adminpanel:populate[10, product, name:name description:lorem price:number]"
     end
 
 
-    it "should generate 20 product records" do
-      Adminpanel::Product.all.count.should eq 20
+    it "should generate 10 product records" do
+      Adminpanel::Product.all.count.should eq 10
     end
 
     it "attributes shouldn't be nil" do
@@ -40,7 +40,7 @@ describe "adminpanel rake task" do
         (Adminpanel::Section.last.name.should eq("Mission")) &&
         (Adminpanel::Section.last.page.should eq("about")) &&
         (Adminpanel::Section.last.key.should eq("mission")) &&
-        (Adminpanel::Section.last.has_description.should be_true) &&
+        (Adminpanel::Section.last.has_description.should be_false) &&
         (Adminpanel::Section.last.has_image.should be_false)
       )
     end
