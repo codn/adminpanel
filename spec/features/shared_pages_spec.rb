9a986a730f9d4c96b485224d7a9ffab259c8dc91
joseramonc <joserracamacho@gmail.com>
2014-04-03 17:10:29 -0600

    comment unaccurate tests

--- spec/features/shared_pages_spec.rb
+++ spec/features/shared_pages_spec.rb
@@ -22,70 +22,76 @@ describe 'shared pages' do
 			it { should have_link('i', adminpanel.edit_product_path(product)) }
 		end
 
-		describe 'new' do
-			let(:category){ Factory(:category) }
-			before do
-				category.id = 1 #to force instantiation and id.
-				visit adminpanel.new_product_path
-			end
+		context 'product' do
 
-			it { should have_title(I18n.t("action.create") + " " + Adminpanel::Product.display_name) }
-
-			it 'should have add remote category button' do
-				should have_selector(
-					'a.btn-info',
-					:text => I18n.t(
-						'other.add',
-						:model => Adminpanel::Category.display_name
-					)
-				)
-			end
-
-			describe 'submtting with invalid information' do
+			describe 'new' do
+				let(:category){ Factory(:category) }
 				before do
-					find("form#new_resource").submit_form!
+					category.id = 1 #to force instantiation and id.
+					visit adminpanel.new_product_path
 				end
 
 				it { should have_title(I18n.t("action.create") + " " + Adminpanel::Product.display_name) }
-				it { should have_selector("div#alerts") }
-			end
 
-			describe 'opening the remote form modal' do
-				before do
-					# save_and_open_page
-					page.find("a##{Adminpanel::Category.name.demodulize.downcase}-modal-link").click
-				end
-
-				it 'should have modal with correct #modal-title' do
-					page.should have_selector(
-					:css,
-					'h3#modal-title',
-					:content => I18n.t('other.add', :model => Adminpanel::Category.display_name)
+				it 'should have add remote category button' do
+					should have_selector(
+						'a.btn-info',
+						:text => I18n.t(
+							'other.add',
+							:model => Adminpanel::Category.display_name
+						)
 					)
 				end
 
-				# describe 'sending the resource with invalid information' do
-				# 	before do
-				# 		find('#new-Category-button').click
-				# 	end
-				# 	it 'should display error message' do
-				# 		page.should have_selector("div#alerts")
-				# 	end
-				# end
+				describe 'submtting with invalid information' do
+					before do
+						find("form#new_resource").submit_form!
+					end
 
-			end
-
-			describe 'submitting with valid information' do
-				before do
-					fill_in "product_name", :with => "product name"
-					fill_in "product_price", :with => "855.5"
-					find(:css, "#product_category_ids_[value='1']").set(true)
-					find(:xpath, "//input[@id='description-field']").set "<p>a little longer text</p>"
-					find("form#new_resource").submit_form!
+					it { should have_title(I18n.t("action.create") + " " + Adminpanel::Product.display_name) }
+					it { should have_selector("div#alerts") }
 				end
 
-				it { should have_content(I18n.t("action.save_success"))}
-				it { Adminpanel::Categorization.count.should equal(1)}
+				# describe 'opening the remote form modal' do
+				# 	before do
+				# 		page.find("a##{Adminpanel::Category.name.demodulize.downcase}-modal-link").click
+				# 	end
+				#
+				# 	it 'should have modal with correct #modal-title' do
+				# 		page.should have_selector(
+				# 		:css,
+				# 		'h3#modal-title',
+				# 		:content => I18n.t('other.add', :model => Adminpanel::Category.display_name)
+				# 		)
+				# 	end
+				#
+				# 	it 'should have the modal with the correct fields' do
+				# 		should have_selector("input#category_name")
+				# 	end
+
+					# describe 'sending the resource remotely with invalid information' do
+					# 	before do
+					# 		find('#new-Category-button').click
+					# 	end
+					# 	it 'should display error message' do
+					# 		page.should have_selector("div#alerts")
+					# 	end
+					# end
+
+				# end
+
+				describe 'submitting with valid information' do
+					before do
+						fill_in "product_name", :with => "product name"
+						fill_in "product_price", :with => "855.5"
+						find(:css, "#product_category_ids_[value='1']").set(true)
+						find(:xpath, "//input[@id='description-field']").set "<p>a little longer text</p>"
+						find("form#new_resource").submit_form!
+					end
+
+					it { should have_content(I18n.t("action.save_success"))}
+					it { Adminpanel::Categorization.count.should equal(1)}
+				end
 			end
 		end
 
