b2c80e783fb9a17e00d6b5bd50b23e87373d84aa
joseramonc <joserracamacho@gmail.com>
2014-05-21 10:54:42 -0500

    remove webkit declaration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,22 +17,6 @@ require 'rake'
 
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
 
-
-class ActiveRecord::Base
-  mattr_accessor :shared_connection
-  @@shared_connection = nil
-
-  def self.connection
-    @@shared_connection || retrieve_connection
-  end
-end
-# Forces all threads to share the same connection. This works on
-# Capybara because it starts the web server in a thread.
-
-Capybara.register_driver :webkit do |app|
-  Capybara::Driver::Webkit.new(app, :ignore_ssl_errors => true)
-end
-
 Capybara.javascript_driver = :webkit
 
 # don't run on the local machine (since we don't have xvfb running locally)
