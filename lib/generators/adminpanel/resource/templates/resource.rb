79d1350035adc0a96cf35e0afc23eaaa577d38a3
joseramonc <joserracamacho@gmail.com>
2014-05-23 19:02:58 -0500

    fix failing test

--- lib/generators/adminpanel/resource/templates/resource.rb
+++ lib/generators/adminpanel/resource/templates/resource.rb
@@ -8,7 +8,7 @@ module Adminpanel
     def self.form_attributes
       [
 <%= indent(get_attribute_hash, 8) + ',' %>
-<%= indent(file_field_hash, 8) if has_gallery? -%>
+<%= indent(file_field_form_hash, 8) if has_gallery? -%>
       ]
     end
 
