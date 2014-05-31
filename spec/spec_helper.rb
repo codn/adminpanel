4a408f494c16bcf3a681336cf403ec10c38a8809
joseramonc <joserracamacho@gmail.com>
2014-05-31 14:13:23 -0500

    protect from forgery

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ RSpec.configure do |config|
 	config.treat_symbols_as_metadata_keys_with_true_values = true
 	config.run_all_when_everything_filtered = true
 	config.filter_run :focus
-	
+
 	ActiveRecord::Base.establish_connection(
 	  :adapter => 'sqlite3',
 	  :database => ':memory:'
