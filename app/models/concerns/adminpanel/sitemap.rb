ff5f1845c6dc00a61dd756e2f8af832dd32d1fc3
joseramonc <joserracamacho@gmail.com>
2014-06-11 22:08:07 -0500

    don't ping ask

--- app/models/concerns/adminpanel/sitemap.rb
+++ app/models/concerns/adminpanel/sitemap.rb
@@ -9,10 +9,10 @@ module Adminpanel
       after_destroy :ping_engines
     end
 
+  private
     def ping_urls
       {
         google: "http://www.google.com/webmasters/tools/ping?sitemap=%s",
-        ask: "http://submissions.ask.com/ping?sitemap=%s",
         bing: "http://www.bing.com/webmaster/ping.aspx?siteMap=%s"
       }
     end
@@ -21,8 +21,9 @@ module Adminpanel
       logger.info Time.now
       ping_urls.each do |name, url|
         request = url % CGI.escape("#{root_url}/sitemap.xml")
+        logger.info root_url
         logger.info "  Pinging #{name} with #{request}"
-        if Rails.env == "production"
+        if Rails.env != "development"
           response = Net::HTTP.get_response(URI.parse(request))
           logger.info "    #{response.code}: #{response.message}"
           logger.info "    Body: #{response.body}"
