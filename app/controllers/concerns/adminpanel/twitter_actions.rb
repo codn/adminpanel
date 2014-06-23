085e127e860a216d8f870385e20cd368f7e232f1
joseramonc <joserracamacho@gmail.com>
2014-06-22 22:53:54 -0500

    divide #twitter_publish in functions

--- app/controllers/concerns/adminpanel/twitter_actions.rb
+++ app/controllers/concerns/adminpanel/twitter_actions.rb
@@ -3,28 +3,31 @@ module Adminpanel
     extend ActiveSupport::Concern
 
     included do
-      before_filter :set_twitter_auths_count, only:[:index, :create, :update, :destroy, :show]
+      before_filter :set_twitter_auths_count, only:[:index, :create, :update, :destroy, :show, :twitter_publish]
     end
 
     def twitter_publish
-      token = Auth.find_by_key('twitter-token')
-      secret = Auth.find_by_key('twitter-secret')
       resource.twitter_message = params[model_name][:twitter_message]
-      if !token.nil? && !secret.nil? && resource.has_valid_tweet?
-        client = ::Twitter::REST::Client.new do |config|
-          config.consumer_key = Adminpanel.twitter_api_key
-          config.consumer_secret = Adminpanel.twitter_api_secret
-          config.access_token = token.value
-          config.access_token_secret = secret.value
-        end
-        # debugger
+      if !@twitter_token.nil? && !@twitter_secret.nil? && resource.has_valid_tweet?
+        client = get_twitter_client
         client.update(resource.twitter_message)
-        flash[:success] = I18n.t('twitter.posted', user: token.name)
-        redirect_to resource
+        flash[:success] = I18n.t('twitter.posted', user: @twitter_token.name)
+      else
+        flash[:error] = I18n.t('twitter.not-posted')
       end
+      redirect_to resource
     end
 
   private
+    def get_twitter_client
+      ::Twitter::REST::Client.new do |config|
+        config.consumer_key = Adminpanel.twitter_api_key
+        config.consumer_secret = Adminpanel.twitter_api_secret
+        config.access_token = @twitter_token.value
+        config.access_token_secret = @twitter_secret.value
+      end
+    end
+
     def set_twitter_auths_count
       @twitter_token = Auth.find_by_key 'twitter-token'
       @twitter_secret = Auth.find_by_key 'twitter-secret'
