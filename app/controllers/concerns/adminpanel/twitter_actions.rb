98297ac14cbed2927a23559575dc7d5cbc906c79
joseramonc <joserracamacho@gmail.com>
2014-06-30 19:59:01 -0500

    remove duplicated token getter

--- app/controllers/concerns/adminpanel/twitter_actions.rb
+++ app/controllers/concerns/adminpanel/twitter_actions.rb
@@ -3,13 +3,13 @@ module Adminpanel
     extend ActiveSupport::Concern
 
     included do
-      before_filter :set_twitter_auths_count, only:[:index, :create, :update, :destroy, :show, :twitter_publish]
+      before_filter :get_twitter_token, only:[:index, :create, :update, :destroy, :show, :twitter_publish]
     end
 
     def twitter_publish
       resource.twitter_message = params[model_name][:twitter_message]
       if !@twitter_token.nil? && !@twitter_secret.nil? && resource.has_valid_tweet?
-        client = get_twitter_client
+        client = get_twitter_token
         client.update(resource.twitter_message)
         flash[:success] = I18n.t('twitter.posted', user: @twitter_token.name)
       else
@@ -19,18 +19,18 @@ module Adminpanel
     end
 
   private
-    def get_twitter_client
-      ::Twitter::REST::Client.new do |config|
-        config.consumer_key = Adminpanel.twitter_api_key
-        config.consumer_secret = Adminpanel.twitter_api_secret
-        config.access_token = @twitter_token.value
-        config.access_token_secret = @twitter_secret.value
-      end
-    end
-
-    def set_twitter_auths_count
+    def get_twitter_token
       @twitter_token = Auth.find_by_key 'twitter-token'
       @twitter_secret = Auth.find_by_key 'twitter-secret'
+
+      if !@twitter_token.nil? && !@twitter_secret.nil?
+        @twitter_client ||= ::Twitter::REST::Client.new do |config|
+          config.consumer_key        = Adminpanel.twitter_api_key
+          config.consumer_secret     = Adminpanel.twitter_api_secret
+          config.access_token        = @twitter_token.value
+          config.access_token_secret = @twitter_secret.value
+        end
+      end
     end
 
     def model_name
