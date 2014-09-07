9bf2ba21fe3aab8dbd78793e5b116621f520ac61
joseramonc <joserracamacho@gmail.com>
2014-09-07 18:42:23 -0500

    :green_heart: fix roles in fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,15 @@
-ENV['RAILS_ENV'] = 'test'
-require 'rails'
-require File.expand_path('../dummy/config/environment', __FILE__)
-require 'rails/test_help'
-require 'minitest/autorun'
-require 'minitest/emoji' #emoji output
-require 'capybara/rails'
-require 'capybara/poltergeist'
-
+ENV["RAILS_ENV"] = "test"
+require "rails"
+require File.expand_path("../dummy/config/environment", __FILE__)
+require "rails/test_help"
+require "minitest/autorun"
+require "minitest/emoji" #emoji output
+require "capybara/rails"
+require "capybara/poltergeist"
 
 Capybara.current_driver = :poltergeist
 
-load Rails.root.join('db', 'schema.rb')
+load Rails.root.join("db", "schema.rb")
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
