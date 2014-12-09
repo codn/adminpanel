06d346ec0b91e2ca6005707cd6f1b3ea7b5087cf
Jose Ramon Camacho <joserracamacho@gmail.com>
2014-12-09 11:01:52 -0600

    :white_check_mark: categories index and create

--- test/features/categories/categories_index_test.rb
+++ test/features/categories/categories_index_test.rb
@@ -7,11 +7,19 @@ class CategoriesIndexTest < ViewCase
   setup :visit_adminpanel_categories_path
   teardown :teardown
 
-  def test_add_category
-    skip
-    ### p page.body
-    ### :(
-    assert_link('Crear Categoria para objeto')
+  def test_adding_invalid_category
+    assert_link('Crear Categoria para test_object')
+    trigger_modal('Crear Categoria para test_object')
+    assert_equal('Crear Categoría', find('#myModalLabel').text)
+    submit_modal 'Agregar Categoria'
+    assert_content( I18n.t('errors', model: 'Categoria', count: 1) )
+  end
+
+  def test_adding_valid_category
+    trigger_modal('Crear Categoria para test_object')
+    fill_in 'category_name', with: 'categories index name'
+    submit_modal 'Agregar Categoria'
+    assert_xpath("//a[contains(text(), 'categories index name' )]")
   end
 
   private
