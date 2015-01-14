1fc9001646640ba10c5ef79ab3ae58952761aa2d
Jose Ramon Camacho <joserracamacho@gmail.com>
2015-01-14 11:18:50 -0600

    support multiple fields in the generator #35

--- lib/generators/adminpanel/contact/contact_generator.rb
+++ lib/generators/adminpanel/contact/contact_generator.rb
@@ -1,12 +1,24 @@
 require 'rails/generators/active_record'
 module Adminpanel
   class ContactGenerator < ActiveRecord::Generators::Base
-    desc "Generate the migrations necessary to start the gem"
-    source_root File.expand_path("../templates", __FILE__)
-    argument :name, :type => :string, :default => "default", :require => false
+    desc 'Generate the contact template to use with a mail form'
+    source_root File.expand_path('../templates', __FILE__)
+    argument :name, type: :string, default: '', required: false
+    argument :fields, type: :array, default: [], required: false
 
     def copy_contact
-      copy_file 'contact_template.rb', 'app/models/contact.rb'
+      fields = extract_fields
+      template 'contact_template.rb', 'app/models/contact.rb'
     end
+
+    private
+      def extract_fields
+        if fields.empty? && name == ''
+          fields << 'email' << 'body'
+        elsif name != ''
+          fields << name
+        end
+        return fields
+      end
   end
 end
--- lib/generators/adminpanel/contact/templates/contact_template.rb
+++ lib/generators/adminpanel/contact/templates/contact_template.rb
@@ -3,12 +3,23 @@ class Contact
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :name, :email, :subject, :body
+  attr_accessor <%= fields.map{|e| ":#{e}" }.join(', ') -%>
 
   validates_presence_of :name, message: "#{I18n.t('model.attributes.name')} #{I18n.t('activerecord.errors.messages.blank')}"
+
+<%- if fields.include?('email') -%>
+  # validations for email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates_format_of :email, :with => VALID_EMAIL_REGEX, message: "#{I18n.t('model.attributes.email')} #{I18n.t('activerecord.errors.messages.invalid')}"
-  validates_presence_of :body, message: "#{I18n.t('model.attributes.body')} #{I18n.t('activerecord.errors.messages.blank')}"
+  validates_format_of :email, with: VALID_EMAIL_REGEX, message: "#{I18n.t('model.attributes.email')} #{I18n.t('activerecord.errors.messages.invalid')}"
+  <%- fields.delete('email') -%>
+<% end -%>
+
+<% fields.each do |f| -%>
+  <%- current_field = f.to_sym -%>
+  # validations for <%= current_field %>
+  validates_presence_of <%= current_field -%>, message: "#{I18n.t('model.attributes.<%= current_field -%>')} #{I18n.t('activerecord.errors.messages.blank')}"
+
+<% end -%>
 
   def initialize(attributes = {})
     attributes.each do |name, value|
@@ -17,7 +28,7 @@ class Contact
   end
 
   def self.display_name
-    'Correo'
+    'Mail'
   end
 
   def self.get_attribute_label(label)
