bf4e1d8f3eddd8b50958bde7389e273b45fb6918
Jose Ramon Camacho <joserracamacho@gmail.com>
2015-03-25 12:41:37 -0600

    add friendlyId concern

--- app/controllers/concerns/adminpanel/sortable_actions.rb
+++ app/controllers/concerns/adminpanel/sortable_actions.rb
@@ -3,16 +3,8 @@ module Adminpanel
     extend ActiveSupport::Concern
 
     def move_to_position
-      if @model.respond_to? :friendly
-        resource = @model.friendly.find(params[:id])
-      else
-        resource = @model.find(params[:id])
-      end
-
-      resource = @model.find(params[:id])
-
-      resource.move_to_position(params[:position].to_i)
-      render json: {status: :ok}
+      @resource_instance.move_to_position(params[:position].to_i)
+      render json: { status: :ok }
     end
   end
 end
