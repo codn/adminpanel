5f8b45f51e679161376978903e56c7b722efc633
Jose Ramon Camacho <joserracamacho@gmail.com>
2017-08-02 11:54:06 -0500

    use visible for travis

--- test/features/shared/ui/filtering_test.rb
+++ test/features/shared/ui/filtering_test.rb
@@ -12,13 +12,13 @@ class FilteringTest < ViewCase
   def test_filters_all_elements_when_nothing_matched
     fill_in 'search-input', with: 'abcdeffj nothing matches this'
     # 1 since search-input is inside an .accordion-group
-    assert page.has_selector?('.accordion-group', count: 1)
+    assert page.has_selector?('.accordion-group', count: 1, visible: true)
   end
 
   def test_filter_to_only_matched_element
     fill_in 'search-input', with: 'pRODuct'
     # 2 since search-input is inside an .accordion-group
-    assert page.has_selector?('.accordion-group', count: 2)
+    assert page.has_selector?('.accordion-group', count: 2, visible: true)
   end
 
   protected
