3e596bf5a098f33f9c4c38978cd180b1eda0cf65
Jose Ramon Camacho <joserracamacho@gmail.com>
2017-08-02 11:52:16 -0500

    change assert for travis ci

--- test/features/shared/ui/filtering_test.rb
+++ test/features/shared/ui/filtering_test.rb
@@ -6,19 +6,19 @@ class FilteringTest < ViewCase
   setup :sign_in
 
   def test_input_is_present
-    assert_selector '#search-input'
+    assert page.has_selector?('#search-input')
   end
 
   def test_filters_all_elements_when_nothing_matched
     fill_in 'search-input', with: 'abcdeffj nothing matches this'
     # 1 since search-input is inside an .accordion-group
-    assert_selector '.accordion-group', count: 1
+    assert page.has_selector?('.accordion-group', count: 1)
   end
 
   def test_filter_to_only_matched_element
     fill_in 'search-input', with: 'pRODuct'
     # 2 since search-input is inside an .accordion-group
-    assert_selector '.accordion-group', count: 2
+    assert page.has_selector?('.accordion-group', count: 2)
   end
 
   protected
